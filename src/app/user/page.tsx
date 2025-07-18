65dc3db8fffe6dedb99cf74f57633fb71f86ff67
rongtaosheng <rongtaosheng@econ-tech.cn>
2025-07-18 11:54:01 +0800

    fix: 用户UI

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,11 +2,98 @@
 
 import React from "react";
 import LayoutFrame from "@/components/LayoutFrame";
+import PersonAddAltTwoToneIcon from "@mui/icons-material/PersonAddAltTwoTone";
+import RoomPreferencesTwoToneIcon from "@mui/icons-material/RoomPreferencesTwoTone";
+import UserTable from "@/components/UserTable";
+import Layout from "@/components/Layout";
+import UserList from "@/components/UserList";
+import RoleList from "@/components/RoleList";
+import RoleTable from "@/components/RoleTable";
+import { Box, Button, Typography } from "@mui/joy";
+import PersonAddTwoToneIcon from "@mui/icons-material/PersonAddTwoTone";
+import ManageAccountsTwoToneIcon from "@mui/icons-material/ManageAccountsTwoTone";
 
 export default function Page() {
+  const [tabBar, setTabBar] = React.useState("user");
   return (
-    <LayoutFrame tabBar={{ items: [], value: "", onChange: () => {} }}>
-      用户管理
+    <LayoutFrame
+      rootProps={{
+        sx: {
+          gridTemplateColumns: {
+            xs: "1fr",
+            md: "160px 1fr",
+          },
+        },
+      }}
+      tabBar={{
+        value: tabBar,
+        items: [
+          {
+            label: "用户",
+            key: "user",
+            icon: <ManageAccountsTwoToneIcon />,
+          },
+          {
+            label: "角色",
+            key: "role",
+            icon: <RoomPreferencesTwoToneIcon />,
+          },
+        ],
+        onChange: (tabBar) => {
+          setTabBar(tabBar);
+        },
+      }}
+    >
+      <Layout.Main
+        sx={{
+          p: 2,
+          position: "relative",
+          display: {
+            xs: "initial",
+            md: "initial",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            mb: 1,
+            gap: 1,
+            flexDirection: { xs: "column", sm: "row" },
+            alignItems: { xs: "start", sm: "center" },
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            level="h2"
+            component="h1"
+            sx={{ display: { xs: "none", sm: "initial" } }}
+          >
+            用户管理
+          </Typography>
+        </Box>
+        {(() => {
+          switch (tabBar) {
+            case "user":
+              return (
+                <>
+                  <UserTable />
+                  <UserList />
+                </>
+              );
+            case "role":
+              return (
+                <>
+                  <RoleTable />
+                  <RoleList />
+                </>
+              );
+            default:
+              return null;
+          }
+        })()}
+      </Layout.Main>
     </LayoutFrame>
   );
 }
